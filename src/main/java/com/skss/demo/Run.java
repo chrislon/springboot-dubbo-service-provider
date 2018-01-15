5dc578ce2e835a11eec0ae274dfb7ecb89d7c1f8
chris <chrislon8850@gmail.com>
2018-01-15 18:21:47 +0800

    rediscluster

--- src/main/java/com/skss/demo/Run.java
+++ src/main/java/com/skss/demo/Run.java
@@ -1,12 +1,13 @@
 package com.skss.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Run {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Run.class, args);
 
 	}
--- src/main/java/com/skss/demo/redis/RedisCache.java
+++ src/main/java/com/skss/demo/redis/RedisCache.java
@@ -1,7 +1,6 @@
 package com.skss.demo.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -9,18 +8,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisCache {
 
-//	@Autowired
-//	private RedisTemplate<String,String> redisTemplate;
 
+	
 	@Autowired
-	private RedisConnectionFactory redisConnectionFactory;
+	private RedisTemplate<String, String> redisTemplate;
+	
 	
 	public void set(String key, String value) {
 		
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
 		
-		redisTemplate.setConnectionFactory(redisConnectionFactory);
-		redisTemplate.afterPropertiesSet();
 		
 		ValueOperations<String, String> opsForValue = redisTemplate.opsForValue();
 		opsForValue.set(key, value);
@@ -28,11 +24,9 @@ public class RedisCache {
 	}
 
 	public String get(String key) {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
 		
-		redisTemplate.setConnectionFactory(this.redisConnectionFactory);
-		redisTemplate.afterPropertiesSet();
 		ValueOperations<String, String> opsForValue = redisTemplate.opsForValue();
 		return opsForValue.get(key);
+		
 	}
 }
